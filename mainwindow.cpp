b6f55b02774b2469514dcd5e9a89bf49ced28bf1
Konrad Mateja <konradmateja65@gmail.com>
2025-12-18 02:16:49 +0100

    move all logic to mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,55 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QFrame>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setWindowFlags(
+        Qt::FramelessWindowHint |
+        Qt::WindowStaysOnTopHint |
+        Qt::Tool
+    );
+    setAttribute(Qt::WA_TranslucentBackground);
+
+    QWidget *centralWidget = new QWidget(this);
+    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+
+    QFrame *backgroundFrame = new QFrame(centralWidget);
+    backgroundFrame->setStyleSheet(
+        "QFrame {"
+        "   background-color: #2D2D2D;"
+        "   border-radius: 15px;"
+        "   border: 1px solid #555;"
+        "}"
+    );
+
+    mainLayout->addWidget(backgroundFrame);
+    setCentralWidget(centralWidget);
+
+    QVBoxLayout *contentLayout = new QVBoxLayout(backgroundFrame);
+    QLabel *label = new QLabel("Wciśnij Ctrl+Shift+K", backgroundFrame);
+    label->setStyleSheet("color: white; font-size: 18px; border: none; background: transparent;");
+    label->setAlignment(Qt::AlignCenter);
+    contentLayout->addWidget(label);
+
+    hotkey = new QHotkey(QKeySequence("Ctrl+Shift+K"), true, this);
+    connect(hotkey, &QHotkey::activated, this, [this]() {
+        if (this->isVisible()) {
+            this->hide();
+        } else {
+            this->show();
+            this->raise();          // Put window on top
+            this->activateWindow(); // Focus on window
+        }
+    });
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QHotkey>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QHotkey *hotkey;
 };
 #endif // MAINWINDOW_H

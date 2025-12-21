a5b6186af57dd8e920c447620fad432662aed9bc
w3rr0 <konradmateja65@gmail.com>
2025-12-21 17:05:18 +0100

    create ollamaclient structure

--- ollamaclient.cpp
+++ ollamaclient.cpp
@@ -1,5 +1,1 @@
-//
-// Created by Komdziu on 21/12/2025.
-//
-
 #include "ollamaclient.h"
--- ollamaclient.h
+++ ollamaclient.h
@@ -1,12 +1,33 @@
-//
-// Created by Komdziu on 21/12/2025.
-//
-
 #ifndef POPCHAT_OLLAMACLIENT_H
 #define POPCHAT_OLLAMACLIENT_H
 
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QVector>
+#include <QString>
 
-class ollamaclient {
+
+struct ChatMessage {
+    QString role;
+    QString content;
+};
+
+class OllamaClient : public QObject {
+    Q_OBJECT
+public:
+    explicit OllamaClient(QObject *parent = nullptr);
+    void sendMessage();
+    void resetConversation();
+signals:
+    void textReceived();
+    void replyFinished();
+private slots:
+    void onReadOnly();
+private:
+    QNetworkAccessManager *networkManager;
+    QNetworkReply *currentReply;
+    QVector<ChatMessage> conversationHistory;
 };
 
 

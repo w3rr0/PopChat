07944897b0c5c9777afdc65589789c5c0b148d22
w3rr0 <konradmateja65@gmail.com>
2025-12-21 17:04:52 +0100

    create ollamaclient

---
+++ ollamaclient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Komdziu on 21/12/2025.
+//
+
+#include "ollamaclient.h"
---
+++ ollamaclient.h
@@ -0,0 +1,13 @@
+//
+// Created by Komdziu on 21/12/2025.
+//
+
+#ifndef POPCHAT_OLLAMACLIENT_H
+#define POPCHAT_OLLAMACLIENT_H
+
+
+class ollamaclient {
+};
+
+
+#endif //POPCHAT_OLLAMACLIENT_H

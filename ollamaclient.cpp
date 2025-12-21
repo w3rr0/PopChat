8fb0d9963c3bb70d670cd58fa1d326953770271e
Konrad Mateja <konradmateja65@gmail.com>
2025-12-21 19:09:49 +0100

    add cmake settings to gitignore and add simple implementation for ollama client cpp

--- ollamaclient.cpp
+++ ollamaclient.cpp
@@ -1,1 +1,18 @@
 #include "ollamaclient.h"
+
+OllamaClient::OllamaClient(QObject *parent) : QObject(parent) {
+	networkManager = new QNetworkAccessManager(this);
+	currentReply = nullptr;
+}
+
+void OllamaClient::sendMessage() {
+	// Implementation of sending a message to the Ollama API
+}
+
+void OllamaClient::resetConversation() {
+	conversationHistory.clear();
+}
+
+void OllamaClient::onReadOnly() {
+	// Implementation of handling read-only response from the API
+}

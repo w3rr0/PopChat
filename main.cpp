82278243a492ad6d62e09cbac521b4e230310080
Konrad Mateja <konradmateja65@gmail.com>
2025-12-23 02:04:16 +0100

    load settings at program start

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
+#include "ollamaclient.h"
 
 #include <QApplication>
 #include <QLabel>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Load Settings
+    QSettings settings("model");
+    const QString savedModel = settings.value("modelName", "").toString();
+    OllamaClient::setModelName(savedModel);
 
     // Creating main window
     MainWindow w;

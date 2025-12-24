07f41e030cdfea068ff2381637a41404225d9fde
w3rr0 <konradmateja65@gmail.com>
2025-12-24 01:12:53 +0100

    display logs from app while running via script

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 # Reload configuration (detect new file)
 echo "--- CMake config ---"
-cmake -B build -DCMAKE_PREFIX_PATH=/Users/mac/Qt/6.10.1/macos
+cmake -B build -DCMAKE_PREFIX_PATH=/Users/mac/Qt/6.10.1/macos -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # Compile
 echo "--- Building project ---"
 if cmake --build build; then
-    echo "--- Run app ---"
+    echo "--- App logs ---"
     # Run - use & to prevent terminal freez
-    ./build/PopChat.app/Contents/MacOS/PopChat &
+    ./build/PopChat.app/Contents/MacOS/PopChat
 else
     echo "[ERROR]: Compilation failed"
 fi

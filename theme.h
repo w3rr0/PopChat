93f95ec41ab5bebb33662bd74a0d6370defe5df7
w3rr0 <konradmateja65@gmail.com>
2025-12-24 21:33:44 +0100

    move color setting to theme.h

---
+++ theme.h
@@ -0,0 +1,24 @@
+#ifndef THEME_H
+#define THEME_H
+
+#include <QString>
+
+namespace Theme {
+    // Background
+    const QString WindowBg = "#404040";
+    const QString Surface = "#2D2D2D";
+    const QString Pressed = "#191919";
+
+    // Accent scheme
+    const QString Accent = "#0078D7";
+    const QString AccentHover = "#0063B1";
+    const QString AccentPressed = "#004C87";
+
+    // Text
+    const QString TextMain = "white";
+    const QString TextUser = "#E0E0E0";
+
+    const QString Response = "#3A3A3A";
+}
+
+#endif // THEME_H
